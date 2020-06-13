fd37c7bef7a3e14c611fa7f9442cef101c2553ac
Sid Mani <sid@sidmani.com>
2020-06-12 23:40:53 -0700

    add state handling

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,10 +1,7 @@
-from .gpio import initGPIO, setLED
+from .gpio import initGPIO, setState, State
 from .thermo import initThermo, temperature
 
 initGPIO()
 initThermo()
 
-setLED(0, True)
-
-while(True):
-    print(temperature())
+setState(State.HEAT)
--- backend/gpio.py
+++ backend/gpio.py
@@ -1,4 +1,12 @@
 import importlib.util
+from enum import Enum
+
+
+class State(Enum):
+    STANDBY = 0
+    HEAT = 1
+
+
 try:
     importlib.util.find_spec('RPi.GPIO')
     import RPi.GPIO as GPIO
@@ -7,6 +15,10 @@ except ImportError:
 
 RELAY = [27, 17, 18, 15, 14, 4, 3, 2]
 LED = [13, 19, 26]
+
+TOP_ELEMENT = RELAY[0]
+BOTTOM_ELEMENT = RELAY[1]
+FAN = RELAY[3]
 
 
 def initGPIO():
@@ -19,5 +31,39 @@ def initGPIO():
         GPIO.setup(l, GPIO.OUT)
         GPIO.output(l, False)
 
-def setLED(i, mode):
-    GPIO.output(LED[i], mode)
+
+def clearLEDs():
+    for l in LED:
+        GPIO.output(l, False)
+
+
+def clearRelays():
+    for r in RELAY:
+        GPIO.output(r, False)
+
+
+def heat():
+    # set the red LED
+    GPIO.output(LED[0], True)
+
+    # enable both heating elements
+    GPIO.output(TOP_ELEMENT, True)
+    GPIO.output(BOTTOM_ELEMENT, True)
+
+    # enable the fan
+    GPIO.output(FAN, True)
+
+
+def standby():
+    # set 3rd LED
+    GPIO.output(LED[2], True)
+
+
+def setState(s):
+    clearLEDs()
+    clearRelays()
+
+    if s == State.STANDBY:
+        standby()
+    elif s == State.HEAT:
+        heat()
