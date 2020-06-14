f6b9277e1c5d6f7bf68c3f04c38bfbf51516a122
Sid Mani <sid@sidmani.com>
2020-06-13 18:31:30 -0700

    foo

--- backend/__init__.py
+++ backend/__init__.py
@@ -85,7 +85,7 @@ async def preheat():
     except Exception:
         pass
 
-    pid = PID(0.02, 0.02, 0.01)
+    pid = PID(0.1, 0.07, 0.01)
     pid.setpoint = 40
     sch.add_job(
         preheatHandler,
