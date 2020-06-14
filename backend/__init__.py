c2711184c6b59b1009e71bd687ce18db76e86107
Sid Mani <sid@sidmani.com>
2020-06-14 12:36:48 -0700

    pid

--- backend/__init__.py
+++ backend/__init__.py
@@ -23,7 +23,7 @@ standby()
 sch = BackgroundScheduler()
 sch.start()
 
-pid = PID(1, 0.01, 1, setpoint=25)
+pid = PID(4, 0.3, 1, setpoint=25)
 pid.proportional_on_measurement = True
 pid.output_limits = (-10, 10)
 
